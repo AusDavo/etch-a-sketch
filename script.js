cc0a29dfde9da14ca6665fd67f2e5c11e2c15b9a
David Pinkerton <david@dpinkerton.com>
2023-01-14 15:50:49 +1000

    Generate new grids but often have residual div squares hang about below

--- script.js
+++ script.js
@@ -18,7 +18,38 @@ squares.forEach(square => {
     square.addEventListener('mouseenter', e => {
         e.target.classList.add('hovered');
     });
-    square.addEventListener('mouseleave', e => {
-        e.target.classList.remove('hovered');
-    });
+//    square.addEventListener('mouseleave', e => {
+//        e.target.classList.remove('hovered');
+//    });
 });
+
+// Listen for click on button and prompt for new dimension
+const dimensionBtn = document.getElementById('dimension-btn');
+dimensionBtn.addEventListener('click', showPrompt);
+
+function showPrompt() {
+  const dimension = prompt("Enter the number of rows or columns:");
+  updateGrid(dimension);
+}
+
+// Update grid based on the new dimension provided by the user
+function updateGrid(dimension) {
+  
+  let hoveredElements = document.querySelectorAll(".hovered");
+  hoveredElements.forEach(function(element) {
+    element.classList.remove("hovered");
+  });
+  
+  squares.forEach(square => square.remove());
+  let totalsquares = dimension * dimension;
+  
+  for (let i = 0; i < totalsquares; i++) {
+    const newSquare = square.cloneNode();
+    container.appendChild(newSquare);
+    container.childNodes[i].style.height = `calc(100% / ${dimension})`;
+    container.childNodes[i].style.width = `calc(100% / ${dimension})`;
+    container.childNodes[i].addEventListener("mouseenter", e => {
+      e.target.classList.add('hovered');
+    });
+  };
+}
