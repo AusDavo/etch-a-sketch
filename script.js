788a4f99a5c9beeb6decabb1c1d60545b42808f5
David Pinkerton <david@dpinkerton.com>
2023-01-14 20:43:51 +1000

    Making new grids works

--- script.js
+++ script.js
@@ -1,55 +1,32 @@
-// Select the container element
-const container = document.querySelector('.grid-container');
-
-// Create a template for a single square div
-const square = document.createElement('div');
-square.classList.add('grid-square');
-
-// Use a loop to create and append 256 square divs to the container
-for (let i = 0; i < 256; i++) {
-    const newSquare = square.cloneNode();
-    container.appendChild(newSquare);
-}
-
-//Add a class to the square element, when the mouse enters and remove the class when the mouse leaves
-const squares = document.querySelectorAll('.grid-square');
-
-squares.forEach(square => {
-    square.addEventListener('mouseenter', e => {
-        e.target.classList.add('hovered');
-    });
-//    square.addEventListener('mouseleave', e => {
-//        e.target.classList.remove('hovered');
-//    });
-});
-
 // Listen for click on button and prompt for new dimension
 const dimensionBtn = document.getElementById('dimension-btn');
 dimensionBtn.addEventListener('click', showPrompt);
 
 function showPrompt() {
-  const dimension = prompt("Enter the number of rows or columns:");
-  updateGrid(dimension);
+  let dimension = prompt("Enter the number of rows or columns:", "16");
+  prepGrid(dimension);
 }
 
 // Update grid based on the new dimension provided by the user
-function updateGrid(dimension) {
+function prepGrid(dimension) {
   
-  let hoveredElements = document.querySelectorAll(".hovered");
-  hoveredElements.forEach(function(element) {
-    element.classList.remove("hovered");
-  });
-  
-  squares.forEach(square => square.remove());
-  let totalsquares = dimension * dimension;
-  
-  for (let i = 0; i < totalsquares; i++) {
-    const newSquare = square.cloneNode();
-    container.appendChild(newSquare);
+  // Select the container element
+  const container = document.querySelector('.grid-container');
+  container.style.display = "flex";
+  container.style.display = "wrap";
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  for (let i = 0; i < dimension * dimension; i++) { 
+    const square = document.createElement("div");
+    container.appendChild(square);
     container.childNodes[i].style.height = `calc(100% / ${dimension})`;
     container.childNodes[i].style.width = `calc(100% / ${dimension})`;
+    container.childNodes[i].classList.add("grid-square");
     container.childNodes[i].addEventListener("mouseenter", e => {
-      e.target.classList.add('hovered');
+    e.target.classList.add('hovered');
     });
   };
 }
